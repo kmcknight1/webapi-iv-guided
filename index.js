82f3d6b134ca08b45d41e99f2e1d4b2264ec9b45
kmcknight1 <katiemcknight1991@gmail.com>
2019-07-11 12:21:51 -0500

    uh

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const server = require("./api/server.js");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const port = 5000 || process.env.PORT;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
